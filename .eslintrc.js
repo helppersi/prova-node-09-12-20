bd9ec0d7cda688823a4b1103fd4083e605be47e7
Raissa Queiroz <raifreelas@gmail.com>
2021-02-02 00:39:05 -0300

    Desafio finalizado '-'

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		commonjs: true,
+		es2020: true,
+		node: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	plugins: ['prettier'],
+	parserOptions: {
+		ecmaVersion: 11,
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		'no-console': 'off',
+		'class-methods-use-this': 'off',
+		'no-param-reassign': 'off',
+		camelcase: 'off',
+		'no-underscore-dangle': 'off',
+		'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+		'newline-after-var': [2, 'always'],
+		'func-names': ['error', 'never'],
+		code: 0,
+		'max-len': ['error', { ignoreStrings: true }],
+		indent: ['error', 'tab'],
+		'linebreak-style': 'off',
+		'no-multiple-empty-lines': 'off',
+		'no-trailing-spaces': ['off', { ignoreComments: 'on' }],
+		'consistent-return': ['error', { treatUndefinedAsUnspecified: true }],
+		'object-shorthand': 'off',
+	},
+};
